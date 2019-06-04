ae83f85f506eb55ef19d41bb5a394d3d27e3a8ba
richwolff <richwolff12@gmail.com>
2019-06-04 16:02:26 -0400

    added cookiecutter

---
+++ superstore/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Exploring the Superstore Dataset For My Blog',
+    author='Rich Wolff',
+    license='MIT',
+)
